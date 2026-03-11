a8f998589baae50eb5a0b0a96561891f15e63c29
André Fortini <124889918+andrefrtn@users.noreply.github.com>
2026-03-11 19:16:30 -0300

    remove lombok

--- Back-end/src/main/java/com/example/invit_in/food/Food.java
+++ Back-end/src/main/java/com/example/invit_in/food/Food.java
@@ -1,13 +1,9 @@
 package com.example.invit_in.food;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 @Table(name = "foods")
 @Entity(name = "foods")
-@Getter
-@Setter
 public class Food {
 
     @Id
@@ -17,4 +13,16 @@ public class Food {
     private String title;
     private String image;
     private Integer price;
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getImage() { return image; }
+    public void setImage(String image) { this.image = image; }
+
+    public Integer getPrice() { return price; }
+    public void setPrice(Integer price) { this.price = price; }
 }
